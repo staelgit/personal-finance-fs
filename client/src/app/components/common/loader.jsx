aab6a9f3a6ca20b815d602d105130be679f49ded
Stahnev Yury <stahnev@stael.ru>
2022-12-13 17:09:01 +0300

    change loader

--- client/src/app/components/common/loader.jsx
+++ client/src/app/components/common/loader.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Loader = () => {
    return (
       <div className="absolute bg-white bg-opacity-60 z-10 h-full w-full flex items-center justify-center">
-         <div className="flex items-center">
-            <span className="text-3xl mr-4">Loading</span>
-
+         <div className="flex items-center flex-col opacity-30">
             <svg
-               className="animate-spin h-5 w-5 text-gray-600 text-primary-dark"
+               className="animate-spin h-10 w-10 text-gray-600 text-primary-dark mb-1.5"
                xmlns="http://www.w3.org/2000/svg"
                fill="none"
                viewBox="0 0 24 24"
@@ -18,7 +16,7 @@ const Loader = () => {
                   cy="12"
                   r="10"
                   stroke="currentColor"
-                  strokeWidth="4"
+                  strokeWidth="3"
                ></circle>
                <path
                   className="opacity-75"
@@ -26,6 +24,7 @@ const Loader = () => {
                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                ></path>
             </svg>
+            <div className="text-sm">loading...</div>
          </div>
       </div>
    );
